24e7dbe2908eec4c44971fc652523fedb76d5f6d
Carol Lee <CLee@thrillworks.com>
2021-02-14 23:04:03 -0500

    Fixed DB connection

--- process.php
+++ process.php
@@ -14,7 +14,7 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 // Connect to DB
-$mysqli = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db) or die(mysqli_error($mysqli));;
+$mysqli = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db) or die(mysqli_error($mysqli));;
 
 $update = false;
 $firstName = '';
